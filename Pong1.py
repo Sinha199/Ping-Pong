fbfe5dacb4d07835a0426bcf7b570ffd510ef0af
Sinha199 <48489081+Sinha199@users.noreply.github.com>
2020-04-29 19:27:39 +0530

    Add files via upload

---
+++ Pong1.py
@@ -0,0 +1,131 @@
+#Getting started with Python3
+#Pong game for implementation
+#Part1(The Interface)
+
+import turtle
+import winsound
+
+wn=turtle.Screen()
+wn.title("Ping-Pong")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+#Score
+score_a=0
+score_b=0
+
+
+
+
+#Game Objects
+
+#PaddleA
+paddle_a=turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+#PaddleB
+paddle_b=turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+#Ball
+ball=turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx=0.5      #2 measures in pixels
+ball.dy=-0.5
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0 Player B: 0",align="center",font=("Courier",24,"normal"))
+#Function
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y=paddle_b.ycor()
+    y+=20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y=paddle_b.ycor()
+    y-=20
+    paddle_b.sety(y)
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress( paddle_b_down,"Down")
+
+#Part2(Main Game Loop)
+while True:
+    wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1 
+        winsound.PlaySound("pong.mp3",winsound.SND_ASYNC)
+    
+    if ball.ycor()<-290:
+        ball.sety(-290)
+        ball.dy *=-1
+        winsound.PlaySound("pong.mp3",winsound.SND_ASYNC) 
+    
+    if ball.xcor()>390:
+        ball.goto(0,0)
+        ball.dx *=-1
+        score_a+=1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b),align="center",font=("Courier",24,"normal"))
+    
+    
+    if ball.xcor()<-390:
+        ball.goto(0,0)
+        ball.dx *=-1
+        score_b+=1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b),align="center",font=("Courier",24,"normal"))
+    #Paddle and ball collisons
+    if (ball.xcor()>340 and ball.xcor()<350) and (ball.ycor()<paddle_b.ycor()+30 and ball.ycor()>paddle_b.ycor()-30):
+        ball.setx(340)
+        ball.dx  *=-1 
+        winsound.PlaySound("pong.mp3",winsound.SND_ASYNC)
+ 
+    if (ball.xcor()<-340 and ball.xcor()>-350) and (ball.ycor()<paddle_a.ycor()+30 and ball.ycor()>paddle_a.ycor()-30):
+        ball.setx(-340)
+        ball.dx  *=-1 
+        winsound.PlaySound("pong.mp3",winsound.SND_ASYNC)
+        
